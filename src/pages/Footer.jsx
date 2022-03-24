049447575d0932fe99dc64ab5edda34bd840ac2a
Lee, Jaehyeok <dlwogur15@gmail.com>
2022-03-24 16:56:09 +0900

    Footer 컴포넌트 구현 (코드 분리)

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css';
+
+
+
+const Footer = () => {
+
+
+    return (
+    <>
+    <footer className='footer'>
+         <div className='margin1'>
+          <span className='span8'>이용약관</span>
+          <span className='span9'>블로그 서비스 운영정책</span>
+          <span className='span10'>개인정보처리방침</span>
+          <span className='span11'>책임의 한계와 법적고지</span>
+          <span className='span12'>글 권리 보호하기</span>
+          <span className='span13'>게시중단요청서비스</span>
+        </div>
+    </footer>
+    </>
+    )
+}
+
+export default Footer;
